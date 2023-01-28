531a66c655e17e7752a860b7c535b36c954048fb
izzelislam <bangfkr002@gmail.com>
2023-01-28 13:20:20 +0700

    first commit

---
+++ app/Models/Donor.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Donor extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'profile_image',
+        'email',
+        'phone_number',
+        'password',
+    ];
+}
